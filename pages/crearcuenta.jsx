f265a4238fc2e65ce8981af5f5cab76991024d47
emmanuel menpe <menpemmanuel30@gmail.com>
2021-12-30 16:18:59 -0600

    validacion de input con formik y yup en login y crearcuenta

--- pages/crearcuenta.jsx
+++ pages/crearcuenta.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import Head from 'next/head';
 import Layout from '../components/Layout';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 const crearcuenta = () => {
+    const formik = useFormik({
+        initialValues:{
+            nombre:'',
+            email:'',
+            password:''
+        },
+        validationSchema: Yup.object({//schema de validacion con un objeto de yup
+            nombre: Yup.string().required('El nombre es obligatorio'),
+            email: Yup.string().email('El email no es valido').required('El email es obligatorio'),
+            password: Yup.string().required('La contraseña es obligatoria').min('Contraseña debe tener minimo 6 caracteres')
+        }),
+        onSubmit: (e) => {//al hacer submit
+            console.log(e);
+        }
+    });
   return (
     <Layout>
-      <div className="md:4/5 xl:w-3/5 mx-auto mb-32">
+        <Head>
+            <title>Crear cuenta</title>
+        </Head>
+        <div className="md:4/5 xl:w-3/5 mx-auto mb-32">
           <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Crear cuenta</h2>
           <div className="flex justify-center mt-5">
             <div className="w-full max-w-lg">
-                <form action="" className="bg-white rounded shadow-md px-8 pt-6 pb-8 md-4">
+                <form 
+                    className="bg-white rounded shadow-md px-8 pt-6 pb-8 md-4"
+                    onSubmit={formik.handleSubmit}
+                >
                     <div className="mb-4">
                         <label 
                             htmlFor="nombre" 
@@ -20,7 +44,16 @@ const crearcuenta = () => {
                             id="nombre" 
                             className="shadow apprearance-none border rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline" 
                             placeholder="Nombre de usuario"
+                            value={formik.values.nombre}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.nombre && formik.errors.nombre? 
+                            <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.nombre}</p>
+                            </div>:null
+                        }
                     </div>
                     <div className="mb-4">
                         <label 
@@ -33,7 +66,16 @@ const crearcuenta = () => {
                             id="email" 
                             className="shadow apprearance-none border rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline" 
                             placeholder="Ingrese su email"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.email && formik.errors.email? 
+                            <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.email}</p>
+                            </div>:null
+                        }
                     </div>
                     <div className="mb-4">
                         <label 
@@ -46,7 +88,16 @@ const crearcuenta = () => {
                             id="password" 
                             className="shadow apprearance-none border rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline" 
                             placeholder="Ingrtese su contraseña"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.password && formik.errors.password? 
+                            <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.password}</p>
+                            </div>:null
+                        }
                     </div>
                     <input 
                         type="submit" 
@@ -56,7 +107,7 @@ const crearcuenta = () => {
                 </form>
             </div>
           </div>
-      </div>
+        </div>
     </Layout>
   )
 };
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Head from 'next/head';
 import Layout from '../components/Layout';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 const login = () => {
+    const formik = useFormik({
+        initialValues:{
+            email:'',
+            password:''
+        },
+        validationSchema: Yup.object({//schema de validacion con un objeto de yup
+            email: Yup.string().email('El email no es valido').required('El email es obligatorio'),
+            password: Yup.string().required('La contraseña es obligatoria')
+        }),
+        onSubmit: (e) => {//al hacer submit
+            console.log(e);
+        }
+    });
+
   return (
     <Layout>
-      <div className="md:4/5 xl:w-3/5 mx-auto mb-32">
+        <Head>
+            <title>Iniciar sesión</title>
+        </Head>
+        <div className="md:4/5 xl:w-3/5 mx-auto mb-32">
           <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Iniciar sesión</h2>
           <div className="flex justify-center mt-5">
             <div className="w-full max-w-lg">
@@ -20,7 +40,16 @@ const login = () => {
                             id="email" 
                             className="shadow apprearance-none border rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline" 
                             placeholder="Ingrese su email"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.email && formik.errors.email? 
+                            <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.email}</p>
+                            </div>:null
+                        }
                     </div>
                     <div className="mb-4">
                         <label 
@@ -33,7 +62,16 @@ const login = () => {
                             id="password" 
                             className="shadow apprearance-none border rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline" 
                             placeholder="Ingrtese su contraseña"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.password && formik.errors.password? 
+                            <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.password}</p>
+                            </div>:null
+                        }
                     </div>
                     <input 
                         type="submit" 
